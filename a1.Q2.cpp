8a0a2e952fcde6e595905ddc5038a92727888087
Sukanya <122634726+Sukanya2311003@users.noreply.github.com>
2023-04-04 22:48:25 +0530

    Update a1.Q2.cpp

--- a1.Q2.cpp
+++ a1.Q2.cpp
@@ -3,22 +3,25 @@ using namespace std;
 int main(int argc, char**argv){
     //grading system
     int marks;
+    cout<<"enter marks out of 100: ";
     cin>>marks;
     if(marks>90 && marks<=100){
         cout<<"Excellent"<<endl;
     }
-    if(marks>80 && marks<=90){
+    else if(marks>80 && marks<=90){
         cout<<"Good"<<endl;
     }
-    if(marks>70 && marks<=80){
+    else if(marks>70 && marks<=80){
         cout<<"Fair"<<endl;
     }
-    if(marks>60 && marks<=70){
+    else if(marks>60 && marks<=70){
         cout<<"meets expectations"<<endl;
     }
-    if(marks<=60) {
+    else if(marks<=60) {
         cout<<"below par";
     }
+    else{
+        cout<<"Invalid input";
+    }
     return 0;
-    
 }
